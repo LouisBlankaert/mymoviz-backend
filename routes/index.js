43ede75d7360af85d35e3e18f4f1aaf1e4479b54
Blankaert <blankaertlouis@outlook.com>
2024-02-08 12:29:36 +0100

    first init

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/movies", (req, res) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      res.json({ movies: data.results });
+      res.json({ movies: data });
     });
 });
 
