341960d33ce8b4c9da321b3530436ce8ab77db21
Blankaert <blankaertlouis@outlook.com>
2024-02-08 11:40:38 +0100

    first init

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+
+const fetch = require("node-fetch");
+
+const TMBD_API_KEY = process.env.TMBD_API_KEY;
+
+router.get("/movies", (req, res) => {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMBD_API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ movies: data.results });
+    });
+});
+
+module.exports = router;
